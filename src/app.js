72696a6c0280333f9cd2d22ef2d65c7654982e00
jorgenetoo <jorgenetoo@yahoo.com.br>
2024-03-06 19:30:58 -0300

    commit

--- src/app.js
+++ src/app.js
@@ -1,26 +1,22 @@
-const express = require("express")
-const routes = require("./routes")
+import express from "express";
+import routes from "./routes";
 
 class App {
     constructor() {
-        this.app = express()
+        this.app = express();
 
-        this.middlewares()
-        this.routes()
-
-
+        this.middlewares();
+        this.routes();
     }
-    middlewares() {
-        this.app.use(express.jason())
 
+    middlewares() {
+        this.app.use(express.json());
     }
 
     routes() {
-    this.app.use(routes)
-
-
+        this.app.use(routes);
     }
 }
 
-module.exports = new App().app
+export default new App().app
 
